4ba20e4375dfe9915a57a81d15db6c99964039c7
yemiancheng <ymc.github@gmail.com>
2020-02-03 08:40:16 +0800

    build: use prettier preset ymc

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,36 @@
 module.exports = {
-    printWidth: 120, // 换行字符串阈值
-    semi: true, // 句末加分号
-    singleQuote: true, // 用单引号
-    trailingComma: 'none', // 最后一个对象元素加逗号
-    bracketSpacing: true, // 对象，数组加空格
-    jsxBracketSameLine: false, // jsx > 是否另起一行
-    arrowParens: 'avoid', // (x) => {} 是否要有小括号
-    requirePragma: false, // 是否要注释来决定是否格式化代码
-    proseWrap: 'preserve' // 是否要换行
+  // 行的长度
+  printWidth: 80,
+  // 缩进符长
+  tabWidth: 2,
+  // 缩进类型
+  useTabs: false,
+  // 句子分号
+  semi: false,
+  // 引号类型
+  singleQuote: true,
+  // 属性引号
+  quoteProps: 'as-needed',
+  // 结尾逗号
+  trailingComma: 'es5',
+  // 大括空格
+  bracketSpacing: true,
+  // 箭头函数
+  arrowParens: 'avoid',
+  // 开始位置
+  rangeStart: 0,
+  // 结束位置
+  rangeEnd: Infinity,
+  // 设解析器：设置用什么来格化
+  // parser: 'babylon',
+  // 需要备注：设置是否开启格化
+  requirePragma: false,
+  // 插入备注：设置是否开启格化
+  insertPragma: false,
+  // 空格敏感
+  htmlWhitespaceSensitivity: 'css',
+  // 行尾字符
+  endOfLine: 'auto',
+  jsxSingleQuote: false,
+  jsxBracketSameLine: true
 };
